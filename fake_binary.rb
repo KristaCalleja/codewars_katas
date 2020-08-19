737ddb38800ed62775daf29db96c852b35be1f68
KristaCalleja <kristacm@gmail.com>
2020-08-18 22:07:07 -0300

    Fake binary kata

---
+++ fake_binary.rb
@@ -0,0 +1,17 @@
+# Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+
+def fake_bin(s)
+    # Insert you code here...
+    p s
+    
+    splitted = s.split(//)
+    splitted.map do |n|
+      if n.to_i >= 5
+        n = '1'
+      else 
+        n = '0'
+      end
+    end.join
+     
+  end
